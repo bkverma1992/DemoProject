f6fd68aa71203e0ddfb2da3e951fae63f16b1b72
Chetan Rajauria <chetanrajauria@Chetans-Air.lan>
2018-11-22 21:23:16 +0530

    Initial commit

---
+++ DemoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoProject
+//
+//  Created by Chetan Rajauria on 22/11/18.
+//  Copyright © 2018 Chetan Rajauria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoProjectTests/DemoProjectTests.m
@@ -0,0 +1,37 @@
+//
+//  DemoProjectTests.m
+//  DemoProjectTests
+//
+//  Created by Chetan Rajauria on 22/11/18.
+//  Copyright © 2018 Chetan Rajauria. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoProjectTests : XCTestCase
+
+@end
+
+@implementation DemoProjectTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
